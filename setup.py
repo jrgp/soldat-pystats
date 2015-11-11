e41e688cc80d616ccbccf2b7629f92f074731bf3
Joe Gillotti <joe@u13.net>
2015-11-10 18:48:10 -0800

    Fix a probably glitch with setup.py ignoring sub packages

--- setup.py
+++ setup.py
@@ -1,12 +1,30 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='piestats',
       version='0.1.0',
-      packages=['piestats'],
+      packages=find_packages(),
       entry_points={
           'console_scripts': [
               'runupdate = piestats.update:main',
               'runsite = piestats.web.site:main',
           ]
-      }
+      },
+      install_requires=[
+          'click==5.1',
+          'flake8==2.5.0',
+          'Flask==0.10.1',
+          'itsdangerous==0.24',
+          'Jinja2==2.8',
+          'MarkupSafe==0.23',
+          'mccabe==0.3.1',
+          'pep8==1.5.7',
+          'pyflakes==1.0.0',
+          'python-dateutil==2.4.2',
+          'python-geoip==1.2',
+          'python-geoip-geolite2==2015.303',
+          'PyYAML==3.11',
+          'redis==2.10.5',
+          'six==1.10.0',
+          'Werkzeug==0.10.4'
+      ]
       )
